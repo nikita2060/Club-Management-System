02b8ab8361d3a2f9d7f0b50ff1b9b827319dcf25
Pushkar Rai <pushkarrai1581@gmail.com>
2025-04-30 17:23:51 +0530

    UI Changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,14 @@ export default function Header() {
   const isLoginPage = location.pathname === '/login';
 
   return (
-    <header className="bg-white py-4 px-6 flex justify-between items-center">
+    <header className="bg-[#b5d4e8] py-4 px-6 flex justify-between items-center">
       <Link to="/" className="flex items-center ">
   <img src="/Images/Logo.png" alt="ClubNect Logo" className="h-12" />
   <span className="text-lg font-semibold">ClubNect</span>
 </Link>
 
       <div className="flex items-center space-x-4">
-        <Link to="/login" className="text-gray-600 hover:text-gray-900">Login</Link>
+        <Link to="/login" className="bg-[#1e293b] text-white px-4 py-2 rounded-md">Login</Link>
         {location.pathname === '/' ? (
   <Link to="/explore" className="bg-[#1e293b] text-white px-4 py-2 rounded-md">
     Explore
